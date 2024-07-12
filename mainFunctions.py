bfb56f8410e69f69e05cf105c61fdfe59ebc226b
Yazan Mahfooz <yazanmahfooz8@gmail.com>
2024-07-12 18:13:47 +0300

    refactored code

--- mainFunctions.py
+++ mainFunctions.py
@@ -3,6 +3,39 @@ from config import *
 from Utils import *
 
 
+def see_black():
+    fir = left_sensor.reflection()
+    sec = right_sensor.reflection()
+    return fir+sec < 20
+
+
+def see_white():
+    ref1 = left_sensor.reflection()
+    ref2 = right_sensor.reflection()
+    return ref1+ref2 > 170
+
+
+def see_white_front():
+    ref1 = front_sensor.reflection()
+    return ref1 > 80
+
+
+def see_yellow_big():
+    ref1 = left_sensor.reflection()
+    ref2 = right_sensor.reflection()
+    return ref1+ref2 > 170
+
+
+def see_yellow_small():
+    h, s, v = left_sensor.hsv()
+    return v > 20
+
+
+def see_yellow_small2():
+    h, s, v = right_sensor.hsv()
+    return v > 20
+
+
 def putBlockOnBlock():
     down_motor.run_time(400, 300*1.5, then=Stop.HOLD, wait=True)
     down_motor.run(600)
@@ -17,67 +50,17 @@ def putBlockOnBlock():
                          then=Stop.HOLD, wait=True)
 
 
-def putBlockOnBlockWithGood(lastWait=True):
-    putBlockOnBlock()
-    up_motor.run_time(speed=-1000, time=600, wait=False)
-    wait(110)
-    make2BlocksGood(lastWait)
-
-
 def make2BlocksGood(lastWait):
     down_motor.run_time(1000, 300*1.5, wait=False)
     base.sync_acc(-35, 1000)
     down_motor.run_time(-1000, 220*3, wait=lastWait)
 
 
-def see_yellow_big():
-    ref1 = left_sensor.reflection()
-    ref2 = right_sensor.reflection()
-    return ref1+ref2 > 170
-
-
-def close_pipe2():
-    reset()
-    down_motor.run_angle(speed=-1000, rotation_angle=250, wait=True)
-    downClaw()
-    base.move_until_method(see_white_front, speed=400)
-    base.move_mm(30, 400)
-    up_motor.run_time(speed=1000, time=100, wait=True)
-    down_motor.run_angle(speed=1000, rotation_angle=100, wait=True)
-    up_motor.run_time(speed=1000, time=800, wait=True)
-
-
-def close_pipe1():
-    reset()
-    downClaw()
-    down_motor.run_angle(speed=-1000, rotation_angle=120, wait=True)
-    base.sync_acc(40)
-    up_motor.run_time(speed=1000, time=800, wait=True)
-    down_motor.run_time(speed=-1000, time=600, wait=True)
-    base.move_mm(40, 330)
-    reset()
-    base.move_mm(40, 330)
-
-
-def see_white():
-    ref1 = left_sensor.reflection()
-    ref2 = right_sensor.reflection()
-    return ref1+ref2 > 170
-
-
-def see_white_front():
-    ref1 = front_sensor.reflection()
-    return ref1 > 80
-
-
-def see_yellow_small():
-    h, s, v = left_sensor.hsv()
-    return v > 20
-
-
-def see_yellow_small2():
-    h, s, v = right_sensor.hsv()
-    return v > 20
+def putBlockOnBlockWithGood(lastWait=True):
+    putBlockOnBlock()
+    up_motor.run_time(speed=-1000, time=600, wait=False)
+    wait(110)
+    make2BlocksGood(lastWait)
 
 
 def take8Blocks():
@@ -120,186 +103,6 @@ def take4block(dis=130):
     base.move_mm(65, 400)
     putBlockOnBlockWithGood(False)
     wait(190)
-
-
-def from_blocks_to_hotam_four(mode):
-    downClaw()
-    base.turn(50)
-    base.move_mm(30, 500)
-    base.move_until_method(see_black, 400)
-    base.move_mm(50, 800)
-    base.turn(120)
-    base.turn_until_method(lambda: right_sensor.reflection() < 7, 100)
-    wait(200)
-    run()
-    base.turn(140)
-    base.sync_acc(200)
-    base.move_until_method(see_yellow_small2, 400)
-    wait(100)
-    resetDetectedColor(170)
-    if (mode == 1):
-        base.sync_acc(250)
-        base.turn(-100)
-        up_motor.run_angle(rotation_angle=50, speed=550)
-        base.turn(100)
-        downClaw()
-        base.sync_acc(280)
-    else:
-        base.sync_acc(530)
-    downMotorResetTrueOrFalse(1000)
-    upClaw()
-    base.sync_acc(-15)
-    base.turn(-100)
-    base.move_mm(200, -1000)
-    up_motor.stop()
-    up_motor.run_angle(rotation_angle=100, speed=-550, wait=True)
-    down_motor.run_time(speed=-400, time=300, wait=True)
-    down_motor.run_time(speed=-1000, time=300)
-    base.sync_acc(200)
-    upMotorResetWithTrueOrFalse(-1000)
-    base.sync_acc(300)
-    base.move_until_method(see_white, 300)
-    downMotorResetTrueOrFalse(1000, False)
-    base.sync_acc(120)
-    base.turn(94)
-    base.sync_acc(440)
-    openpipfun()
-    base.turn(-30)
-    upMotorResetWithTrueOrFalse(-1000)
-    base.turn(-90)
-    base.sync_acc(250)
-
-
-def goToFirstSafahAfterHotam():
-    base.sync_acc(-400)
-    base.stop()
-    down_motor.run_time(speed=1000, time=700, wait=False)
-    right_motor.run_time(speed=-300, time=400)
-    base.sync_acc(-400)
-
-
-def openpipfun():
-    down_motor.run_time(speed=-500, time=450, wait=True)
-    up_motor.run_angle(speed=1000, rotation_angle=200, wait=False)
-    base.stop()
-    left_motor.run_angle(rotation_angle=40, speed=1000)
-    base.move_mm(20, 400)
-
-
-def goToLineAfterSafah():
-    downClaw()
-    base.sync_acc(140)
-    base.turn(70)
-    base.move_mm(20, 300)
-    down_motor.run_time(speed=-400, time=300)
-    base.turn_until_method(lambda: front_sensor.reflection() < 15, speed=100)
-    line.correct(duration=0.35)
-    line.until_method(see_yellow_big, speed=50)
-
-
-def move_steering(angle, steering, speed=1000):
-    motor1 = left_motor
-    motor2 = right_motor
-    if (steering < 0):
-        motor1 = right_motor
-        motor2 = left_motor
-    k = 1-abs(steering)/50
-    print(k)
-    motor1.run_angle(speed, angle, wait=False)
-    motor2.run_angle(speed*k, angle*abs(k), wait=True)
-
-
-def resetStart():
-    down_motor.run_time(speed=1000, time=600, wait=False)
-    up_motor.run_time(speed=-1000, time=600, wait=False)
-
-
-def move_from_blocks_to_line():
-    wait(100)
-    base.stop()
-    base.settings(turn_acceleration=600, turn_rate=10000)
-    base.turn(-90)
-    up_motor.run_time(speed=-300, time=700, wait=False)
-    down_motor.run_time(speed=1000, time=600, wait=False)
-    base.sync_acc(100, acc=1000)
-    base.move_until_method(see_black, speed=350)
-    base.move_mm(distance_in_mm=100, speed=500)
-    base.turn(65)
-    base.turn_until_method(lambda: right_sensor.reflection() < 15, speed=140)
-    line.correct()
-    line.follow_cm(12)
-    line.until_method(see_white)
-    base.sync_acc(225)
-    down_motor.run_angle(speed=-1000, rotation_angle=160, wait=False)
-    wait(100)
-    base.turn(90)
-    up_motor.run_time(speed=1000, time=500, wait=False)
-    wait(200)
-    base.sync_acc(275)
-    down_motor.run_time(speed=1000, time=400, wait=False)
-    up_motor.run_time(speed=-1000, time=700, wait=True)
-    base.stop()
-    left_motor.run_time(speed=400, time=200)
-    base.move_until_method(see_black, 350)
-    base.sync_acc(100)
-    wait(100)
-    base.turn(70)
-    wait(100)
-    base.turn_until_method(lambda: left_sensor.reflection() > 80, speed=120)
-    base.turn_until_method(lambda: left_sensor.reflection() < 30, speed=120)
-    line.correct()
-    down_motor.run_time(speed=-1000, time=400, wait=False)
-    line.follow_cm(32)
-
-
-def resetDetectedColor(angle=191):
-    down_motor.run_angle(speed=-10000, rotation_angle=angle,
-                         then=Stop.HOLD, wait=False)
-
-
-def akbs():
-    down_motor.run_angle(400, 150)
-    base.sync_acc(-40)
-    upClaw()
-    reset()
-    up_motor.run_time(speed=-1000, time=640, wait=False)
-    down_motor.run_time(speed=-1000, time=640)
-    reset()
-
-
-def downClaw(duty=70):
-    up_motor.run_until_stalled(-6000, then=Stop.HOLD, duty_limit=duty)
-
-
-def upClaw():
-    up_motor.run_until_stalled(1000, then=Stop.HOLD, duty_limit=110)
-
-
-def moveUntilBlock(speed):
-    base.move_until_method(lambda: front_sensor.rgb()[0] > 4, speed)
-
-
-def reset():
-    up_motor.run(1000)
-    down_motor.run(1000)
-    up_motor.run_until_stalled(6000, then=Stop.HOLD, duty_limit=75)
-    wait(200)
-    down_motor.hold()
-    up_motor.hold()
-
-
-def upMotorResetWithTrueOrFalse(speed=1000, bool=True):
-    up_motor.run_time(speed=speed, time=700, wait=bool)
-
-
-def downMotorResetTrueOrFalse(speed=1000, bool=True):
-    down_motor.run_time(speed=speed, time=700, wait=bool)
-
-
-def see_black():
-    fir = left_sensor.reflection()
-    sec = right_sensor.reflection()
-    return fir+sec < 20
 
 
 def make2buildRedAndYellow(mode):
@@ -354,3 +157,171 @@ def leaveblocks():
     up_motor.run_time(-250, 340)
     down_motor.run_angle(speed=-1000, rotation_angle=100)
     base.sync_acc(-200)
+
+
+
+def from_blocks_to_hotam_four(mode):
+    downClaw()
+    base.turn(50)
+    base.move_mm(30, 500)
+    base.move_until_method(see_black, 400)
+    base.move_mm(50, 800)
+    base.turn(120)
+    base.turn_until_method(lambda: right_sensor.reflection() < 7, 100)
+    wait(200)
+    run()
+    base.turn(140)
+    base.sync_acc(200)
+    base.move_until_method(see_yellow_small2, 400)
+    wait(100)
+    resetDetectedColor(170)
+    if (mode == 1):
+        base.sync_acc(250)
+        base.turn(-100)
+        up_motor.run_angle(rotation_angle=50, speed=550)
+        base.turn(100)
+        downClaw()
+        base.sync_acc(280)
+    else:
+        base.sync_acc(530)
+    downMotorResetTrueOrFalse(1000)
+    upClaw()
+    base.sync_acc(-15)
+    base.turn(-100)
+    base.move_mm(200, -1000)
+    up_motor.stop()
+    up_motor.run_angle(rotation_angle=100, speed=-550, wait=True)
+    down_motor.run_time(speed=-400, time=300, wait=True)
+    down_motor.run_time(speed=-1000, time=300)
+    base.sync_acc(200)
+    upMotorResetWithTrueOrFalse(-1000)
+    base.sync_acc(300)
+    base.move_until_method(see_white, 300)
+    downMotorResetTrueOrFalse(1000, False)
+    base.sync_acc(120)
+    base.turn(94)
+    base.sync_acc(440)
+    openpipfun()
+    base.turn(-30)
+    upMotorResetWithTrueOrFalse(-1000)
+    base.turn(-90)
+    base.sync_acc(250)
+
+
+def openpipfun():
+    down_motor.run_time(speed=-500, time=450, wait=True)
+    up_motor.run_angle(speed=1000, rotation_angle=200, wait=False)
+    base.stop()
+    left_motor.run_angle(rotation_angle=40, speed=1000)
+    base.move_mm(20, 400)
+
+
+def move_steering(angle, steering, speed=1000):
+    motor1 = left_motor
+    motor2 = right_motor
+    if (steering < 0):
+        motor1 = right_motor
+        motor2 = left_motor
+    k = 1-abs(steering)/50
+    print(k)
+    motor1.run_angle(speed, angle, wait=False)
+    motor2.run_angle(speed*k, angle*abs(k), wait=True)
+
+
+def moveUntilBlock(speed):
+    base.move_until_method(lambda: front_sensor.rgb()[0] > 4, speed)
+
+
+def resetDetectedColor(angle=191):
+    down_motor.run_angle(speed=-10000, rotation_angle=angle,
+                         then=Stop.HOLD, wait=False)
+
+def resetStart():
+    down_motor.run_time(speed=1000, time=600, wait=False)
+    up_motor.run_time(speed=-1000, time=600, wait=False)
+
+
+def downClaw(duty=70):
+    up_motor.run_until_stalled(-6000, then=Stop.HOLD, duty_limit=duty)
+
+
+def upClaw():
+    up_motor.run_until_stalled(1000, then=Stop.HOLD, duty_limit=110)
+
+
+def upMotorResetWithTrueOrFalse(speed=1000, bool=True):
+    up_motor.run_time(speed=speed, time=700, wait=bool)
+
+
+def downMotorResetTrueOrFalse(speed=1000, bool=True):
+    down_motor.run_time(speed=speed, time=700, wait=bool)
+
+
+def reset():
+    up_motor.run(1000)
+    down_motor.run(1000)
+    up_motor.run_until_stalled(6000, then=Stop.HOLD, duty_limit=75)
+    wait(200)
+    down_motor.hold()
+    up_motor.hold()
+
+
+def close_pipe2():
+    reset()
+    down_motor.run_angle(speed=-1000, rotation_angle=250, wait=True)
+    downClaw()
+    base.move_until_method(see_white_front, speed=400)
+    base.move_mm(30, 400)
+    up_motor.run_time(speed=1000, time=100, wait=True)
+    down_motor.run_angle(speed=1000, rotation_angle=100, wait=True)
+    up_motor.run_time(speed=1000, time=800, wait=True)
+
+
+def close_pipe1():
+    reset()
+    downClaw()
+    down_motor.run_angle(speed=-1000, rotation_angle=120, wait=True)
+    base.sync_acc(40)
+    up_motor.run_time(speed=1000, time=800, wait=True)
+    down_motor.run_time(speed=-1000, time=600, wait=True)
+    base.move_mm(40, 330)
+    reset()
+    base.move_mm(40, 330)
+
+def move_from_blocks_to_line():
+    wait(100)
+    base.stop()
+    base.settings(turn_acceleration=600, turn_rate=10000)
+    base.turn(-90)
+    up_motor.run_time(speed=-300, time=700, wait=False)
+    down_motor.run_time(speed=1000, time=600, wait=False)
+    base.sync_acc(100, acc=1000)
+    base.move_until_method(see_black, speed=350)
+    base.move_mm(distance_in_mm=100, speed=500)
+    base.turn(65)
+    base.turn_until_method(lambda: right_sensor.reflection() < 15, speed=140)
+    line.correct()
+    line.follow_cm(12)
+    line.until_method(see_white)
+    base.sync_acc(225)
+    down_motor.run_angle(speed=-1000, rotation_angle=160, wait=False)
+    wait(100)
+    base.turn(90)
+    up_motor.run_time(speed=1000, time=500, wait=False)
+    wait(200)
+    base.sync_acc(275)
+    down_motor.run_time(speed=1000, time=400, wait=False)
+    up_motor.run_time(speed=-1000, time=700, wait=True)
+    base.stop()
+    left_motor.run_time(speed=400, time=200)
+    base.move_until_method(see_black, 350)
+    base.sync_acc(100)
+    wait(100)
+    base.turn(70)
+    wait(100)
+    base.turn_until_method(lambda: left_sensor.reflection() > 80, speed=120)
+    base.turn_until_method(lambda: left_sensor.reflection() < 30, speed=120)
+    line.correct()
+    down_motor.run_time(speed=-1000, time=400, wait=False)
+    line.follow_cm(32)
+
